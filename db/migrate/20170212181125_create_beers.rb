605a75a0a62d8fb8087260f4c16fd84f99431492
sagar-miglani <smiglani1994@gmail.com>
2017-02-12 10:11:39 -0800

    created model

---
+++ db/migrate/20170212181125_create_beers.rb
@@ -0,0 +1,12 @@
+class CreateBeers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :style
+      t.string :hop
+      t.string :alcohol
+
+      t.timestamps
+    end
+  end
+end
